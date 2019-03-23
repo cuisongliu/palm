64269600180e63dcc463f1a129df63bad74e0a72
cuisongliu <cuisongliu@qq.com>
2019-03-23 19:13:57 +0800

    update fist config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 export default {
     data() {
         return {
-            fistRBACServer: "http://fist.lameleg.com:32203/login",
-            fistTerminalServer: "http://fist.lameleg.com:32202/terminal",
+            fistRBACServer: "http://fist.lameleg.com:32204/login",
+            fistTerminalServer: "http://fist.lameleg.com:32203/terminal",
             fistAuthServerToken: "https://fist.lameleg.com:32201/token"
         }
     }
